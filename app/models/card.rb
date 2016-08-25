5dd5b0ca883451c551d469f19188adfa0121c147
mgrassi12 <mgrassi@dius.com.au>
2016-08-25 13:57:52 +1000

    move methods from card.rb into cardservice numerable class, for find_list_name inline find_board rename me to member

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,32 +1,8 @@
 require 'trello'
 
 class Card
-  def self.find_trello_data
-    Trello::Member.find('***REMOVED***')
-  end
-
-  def self.find_board(me)
-    me.boards.first
-  end
-
-  def self.find_cards(board)
-    cards = Array.new
-    board.cards.collect do |card|
-      card_struct = OpenStruct.new
-      card_struct.name = card.name
-      card_struct.id = card.id
-      card_struct.list_id = card.list_id
-      card_struct.list_name = Card.find_list_name(board, card.list_id)
-      cards << card_struct
-      end
-    return cards
-  end
-
-  def self.find_list_name(board, list_id)
-    board.lists.collect do |list|
-      if list.id == list_id
-        return list.name
-      end
-    end
-  end
+  class_attribute :name
+  class_attribute :id
+  class_attribute :list_id
+  class_attribute :list_name
 end
