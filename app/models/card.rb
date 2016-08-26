eb96216d8b865d0a78d17fd117b23324e988ea44
mgrassi12 <mgrassi@dius.com.au>
2016-08-26 17:35:30 +1000

    added start date column, need to fix up failing tests

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,6 +5,7 @@ class Card
   attr_reader :list_id
   attr_reader :name
   attr_reader :list_name
+  attr_reader :start_date
 
   def initialize(board, name, id, list_id)
     @name = name
@@ -12,5 +13,16 @@ class Card
     @list_id = list_id
     list = board.lists.find { |list| list.id == list_id }
     @list_name = list.name
+    # binding.pry
+    action = board.actions.find { |action| (action.type == 'createCard') && (action.data['list']['name'].include?('Resumes to be Screened')) && (action.data['card']['id'] == id) }
+    if action.nil?
+      action = board.actions.find { |action| (action.type == 'updateCard') && (action.data['listAfter']['name'].include?('Resumes to be Screened')) && (action.data['card']['id'] == id) }
+    end
+    if action.nil?
+      # TODO consider using structs
+      action = OpenStruct.new
+      action.date = 'This card has never been placed in the Resumes to be Screened lane.'
+    end
+    @start_date = action.date
   end
 end
