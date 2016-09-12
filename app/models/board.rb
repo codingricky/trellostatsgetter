b564e30972d23b6521c2fa7644f8f6eb8537ac04
mgrassi12 <mgrassi@dius.com.au>
2016-09-12 12:03:48 +1000

    pushed board name to .env, changed this where appropriate

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,6 +2,10 @@ class Board
   attr_accessor :cards
   attr_accessor :lists
   attr_accessor :actions
+  attr_accessor :name
   #always parse into these with an array
   #e.g. Board.actions = [ action1, action2 ]
+  def initialize
+    @name = ENV['BOARD_NAME']
+  end
 end
