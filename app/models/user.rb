19c054079eec46c15d4d4d6b23c91fbdb96bb6ac
mgrassi12 <mgrassi@dius.com.au>
2016-09-27 10:58:40 +1000

    created new todos

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     #TODO test to ensure user is created (exist, return / doesnt, create)
-    #TODO health check
     where(provider: auth.provider, uid: auth.uid.to_s).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
