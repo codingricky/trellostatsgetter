6b1b585d32a5cc340f9795c2ca00c15971940a6f
Ricky Yim <ricky@dius.com.au>
2016-10-22 13:48:42 +1100

    added datatable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.turbolinks
-//= require bootstrap-sprockets
-//= require turbolinks
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require_tree .
+
---
+++ app/assets/javascripts/cards.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $('#trellotable').DataTable();
+});
