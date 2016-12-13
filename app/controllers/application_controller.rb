e2a798d5e285717d585b0390f4165903d6c03378
mgrassi12 <mgrassi@dius.com.au>
2016-12-13 17:03:55 +1100

    Authorization is not required on the update action.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, :except => [:download] unless Rails.env.test?
+  before_action :authenticate_user!, :except => [:download, :update] unless Rails.env.test?
 end
