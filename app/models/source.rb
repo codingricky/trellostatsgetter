1f246df45850d71b1298a2426ddebe8a5a91f7ef
Ricky Yim <ricky@dius.com.au>
2016-12-22 22:39:07 +1100

    added in rails admin

--- app/models/source.rb
+++ app/models/source.rb
@@ -4,8 +4,8 @@ class Source < ApplicationRecord
     matches = value.downcase.include?(self.name.downcase)
     return true if matches
 
-    self.keywords.split(' ').each do |keyword|
-      return true if value.downcase.include?(keyword)
+    self.keywords.split(',').each do |keyword|
+      return true if value.downcase.include?(keyword.downcase)
     end
 
     false
