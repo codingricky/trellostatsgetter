4ef11b9de8893a42b5364354404a22795cd00575
Ricky Yim <ricky@dius.com.au>
2016-12-24 17:18:20 +1100

    added stats page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,18 @@
 class StatsController < ApplicationController
   def index
+    @stats = []
+    Board.all.each do |board|
+      Source.all.each do |source|
+        cards = DownloadedCard.where(source: source.name, location: board.location)
+        no_of_hired = cards.to_a.count{|card| card.is_hired?}
+        no_of_non_hired = cards.size - no_of_hired
+        @stats << OpenStruct.new(location: board.location,
+                                 source: source.name,
+                                 no_of_hired: no_of_hired,
+                                 no_of_non_hired: no_of_non_hired,
+                                 total: cards.size)
+      end
+    end
 
   end
 end
