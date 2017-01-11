d20eeed9287ae69ed5ab2fe1f6387929b31b5b47
Ricky <ricky@dius.com.au>
2017-01-11 11:40:36 +1100

    updated table

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,9 +1,12 @@
 class StatsController < ApplicationController
+
   def index
+    @days = params[:days] || 90
     @stats = []
     Board.all.each do |board|
       Source.all.each do |source|
         cards = DownloadedCard.where(source: source.name, location: board.location)
+        cards = cards.find_all { |card| card.younger_than(@days)}
         no_of_hired = cards.to_a.count{|card| card.is_hired?}
         no_of_non_hired = cards.size - no_of_hired
         @stats << OpenStruct.new(location: board.location,
@@ -13,6 +16,6 @@ class StatsController < ApplicationController
                                  total: cards.size)
       end
     end
-
   end
+
 end
